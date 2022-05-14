23a5691c05ddb25a87711625a5f04796c79db79f
wangqi <1942460489@qq.com>
2022-05-14 23:26:18 +0800

    rename breeze to scaleph.

--- breeze-ui/src/app/pages/opscenter/opscenter.component.ts
+++
@@ -1,7 +0,0 @@
-import { Component } from '@angular/core';
-
-@Component({
-  selector: 'app-opscenter',
-  template: ` <router-outlet></router-outlet> `,
-})
-export class OpsCenterComponent {}
--- breeze-ui/src/app/pages/studio/data-board/data-board.component.html
+++
@@ -1,1 +0,0 @@
-<h1>comming soon</h1>
