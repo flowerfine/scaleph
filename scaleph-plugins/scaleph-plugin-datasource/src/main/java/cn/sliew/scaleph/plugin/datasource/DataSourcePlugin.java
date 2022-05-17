3e502b1cfc786f1aa60d7adba3c79b8591412f56
wangqi <1942460489@qq.com>
2022-05-17 21:37:59 +0800

    README.md

--- scaleph-plugins/scaleph-plugin-datasource/src/main/java/cn/sliew/scaleph/plugin/datasource/DataSourcePlugin.java
+++ scaleph-plugins/scaleph-plugin-datasource/src/main/java/cn/sliew/scaleph/plugin/datasource/DataSourcePlugin.java
@@ -2,9 +2,12 @@ package cn.sliew.scaleph.plugin.datasource;
 
 import cn.sliew.scaleph.plugin.framework.core.Plugin;
 
+import javax.sql.DataSource;
+
 /**
  * datasource plugin demo
  */
 public interface DataSourcePlugin extends Plugin {
 
+    DataSource getDataSource();
 }
---
+++ scaleph-plugins/scaleph-plugin-datasource/src/main/java/cn/sliew/scaleph/plugin/datasource/mysql/MysqlDataSourcePlugin.java
@@ -0,0 +1,54 @@
+package cn.sliew.scaleph.plugin.datasource.mysql;
+
+import cn.sliew.scaleph.plugin.datasource.DataSourcePlugin;
+import cn.sliew.scaleph.plugin.framework.core.PluginInfo;
+import cn.sliew.scaleph.plugin.framework.property.PropertyDescriptor;
+import cn.sliew.scaleph.plugin.framework.property.ValidationResult;
+
+import javax.sql.DataSource;
+import java.util.Collection;
+import java.util.List;
+import java.util.Properties;
+
+public class MysqlDataSourcePlugin implements DataSourcePlugin {
+
+    private static final String PLUGIN_NAME = "MySQLDataSource";
+    private static final String PLUGIN_DESCRIPTION = "MySQL DataSource";
+    private static final String PLUGIN_VERSION = "1.0.0";
+
+    private final PluginInfo pluginInfo;
+
+    public MysqlDataSourcePlugin() {
+        this.pluginInfo = new PluginInfo(PLUGIN_NAME, PLUGIN_DESCRIPTION, PLUGIN_VERSION, this.getClass().getName());
+    }
+
+    @Override
+    public DataSource getDataSource() {
+        return null;
+    }
+
+    @Override
+    public PluginInfo getPluginInfo() {
+        return pluginInfo;
+    }
+
+    @Override
+    public PropertyDescriptor getPropertyDescriptor(String name) {
+        return null;
+    }
+
+    @Override
+    public List<PropertyDescriptor> getRequiredProperties() {
+        return null;
+    }
+
+    @Override
+    public List<PropertyDescriptor> getOptionalProperties() {
+        return null;
+    }
+
+    @Override
+    public Collection<ValidationResult> validate(Properties properties) {
+        return null;
+    }
+}
