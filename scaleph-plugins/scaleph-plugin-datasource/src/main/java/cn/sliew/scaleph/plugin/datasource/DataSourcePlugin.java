f74962379d72a71789f569b0dadf936895f07494
wangqi <1942460489@qq.com>
2022-05-19 08:51:22 +0800

    feature: datasource plugins manager.

--- scaleph-plugins/scaleph-plugin-datasource/src/main/java/cn/sliew/scaleph/plugin/datasource/DataSourcePlugin.java
+++ scaleph-plugins/scaleph-plugin-datasource/src/main/java/cn/sliew/scaleph/plugin/datasource/DataSourcePlugin.java
@@ -3,11 +3,15 @@ package cn.sliew.scaleph.plugin.datasource;
 import cn.sliew.scaleph.plugin.framework.core.Plugin;
 
 import javax.sql.DataSource;
+import java.util.Properties;
+import java.util.Set;
 
 /**
- * datasource plugin demo
+ * datasource plugin
  */
-public interface DataSourcePlugin extends Plugin {
+public interface DataSourcePlugin<T> extends Plugin {
 
-    DataSource getDataSource();
+    T getDataSource();
+
+    void setAdditionalProperties(Properties properties);
 }
