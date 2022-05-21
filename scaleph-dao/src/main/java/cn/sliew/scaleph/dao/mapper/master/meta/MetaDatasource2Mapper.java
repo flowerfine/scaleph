f44111f8a339c70619eb29e1627535d6b7856d11
wangqi <1942460489@qq.com>
2022-05-21 23:54:15 +0800

    feature: meta datasource 2 service.

---
+++ scaleph-dao/src/main/java/cn/sliew/scaleph/dao/mapper/master/meta/MetaDatasource2Mapper.java
@@ -0,0 +1,11 @@
+package cn.sliew.scaleph.dao.mapper.master.meta;
+
+import cn.sliew.scaleph.dao.entity.master.meta.MetaDatasource2;
+import com.baomidou.mybatisplus.core.mapper.BaseMapper;
+
+/**
+ * 元数据-数据源信息2 Mapper 接口
+ */
+public interface MetaDatasource2Mapper extends BaseMapper<MetaDatasource2> {
+
+}
---
+++ scaleph-meta/src/main/java/cn/sliew/scaleph/meta/service/MetaDatasource2Service.java
@@ -0,0 +1,5 @@
+package cn.sliew.scaleph.meta.service;
+
+public interface MetaDatasource2Service {
+
+}
