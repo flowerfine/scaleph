efb6680e480ab74674ebbcbecfe86f05f5f4c767
wangqi <1942460489@qq.com>
2022-05-22 07:12:32 +0800

    feature: meta datasource 2 service.

--- scaleph-meta/src/main/java/cn/sliew/scaleph/meta/service/MetaDatasource2Service.java
+++ scaleph-meta/src/main/java/cn/sliew/scaleph/meta/service/MetaDatasource2Service.java
@@ -1,5 +1,8 @@
 package cn.sliew.scaleph.meta.service;
 
+import cn.sliew.scaleph.meta.service.dto.MetaDatasourceDTO2;
+
 public interface MetaDatasource2Service {
 
+    int insert(MetaDatasourceDTO2 metaDatasourceDTO2);
 }
--- scaleph-meta/src/main/java/cn/sliew/scaleph/meta/service/dto/MetaDatasourceDTO2.java
+++ scaleph-meta/src/main/java/cn/sliew/scaleph/meta/service/dto/MetaDatasourceDTO2.java
@@ -1,28 +1,36 @@
-package cn.sliew.scaleph.dao.entity.master.meta;
+package cn.sliew.scaleph.meta.service.dto;
 
+import cn.sliew.scaleph.common.dto.BaseDTO;
 import cn.sliew.scaleph.dao.entity.BaseDO;
 import com.baomidou.mybatisplus.annotation.TableName;
 import io.swagger.annotations.ApiModel;
 import io.swagger.annotations.ApiModelProperty;
 import lombok.Data;
 import lombok.EqualsAndHashCode;
+import org.hibernate.validator.constraints.Length;
+
+import javax.validation.constraints.NotBlank;
 
 /**
  * 元数据-数据源信息2
  */
 @Data
 @EqualsAndHashCode(callSuper = true)
-@TableName("meta_datasource2")
 @ApiModel(value = "MetaDatasource2对象", description = "元数据-数据源信息2")
-public class MetaDatasource2 extends BaseDO {
+public class MetaDatasourceDTO2 extends BaseDTO {
 
     private static final long serialVersionUID = 1L;
 
+    @NotBlank
     @ApiModelProperty(value = "名称")
     private String name;
 
+    @NotBlank
     @ApiModelProperty(value = "版本")
     private String version;
+
+    @ApiModelProperty(value = "备注描述")
+    private String remark;
 
     @ApiModelProperty(value = "数据源支持的属性")
     private String props;
@@ -30,6 +38,4 @@ public class MetaDatasource2 extends BaseDO {
     @ApiModelProperty(value = "数据源支持的额外属性。")
     private String additionalProps;
 
-    @ApiModelProperty(value = "备注描述")
-    private String remark;
 }
