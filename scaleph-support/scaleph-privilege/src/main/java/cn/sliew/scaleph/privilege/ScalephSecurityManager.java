b585cf3ae80d0aad0564f2e89428d5c6c90cfba5
gleiyu <gleiyu@sina.cn>
2022-05-24 23:10:34 +0800

    feature: prevent System.exit() invocation

--- scaleph-support/scaleph-privilege/src/main/java/cn/sliew/scaleph/privilege/ScalephSecurityManager.java
+++ scaleph-support/scaleph-privilege/src/main/java/cn/sliew/scaleph/privilege/ScalephSecurityManager.java
@@ -1,10 +1,20 @@
 package cn.sliew.scaleph.privilege;
 
+import java.security.Permission;
+
 public class ScalephSecurityManager extends SecurityManager {
 
     @Override
     public void checkExit(int status) {
         super.checkExit(status);
         throw new SecurityException();
+    }
+
+    @Override
+    public void checkPermission(Permission perm) {
+    }
+
+    @Override
+    public void checkPermission(Permission perm, Object context) {
     }
 }
