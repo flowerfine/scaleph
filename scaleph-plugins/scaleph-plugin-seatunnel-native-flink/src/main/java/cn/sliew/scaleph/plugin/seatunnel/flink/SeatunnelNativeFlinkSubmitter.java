0a000acf9c05bd8ae79aed47504ac343c6c9569f
wangqi <1942460489@qq.com>
2022-05-29 22:59:05 +0800

    feature: seatunnel native flink connector.

--- scaleph-plugins/scaleph-plugin-seatunnel-native-flink/src/main/java/cn/sliew/scaleph/plugin/seatunnel/flink/SeatunnelNativeFlinkSubmitter.java
+++ scaleph-plugins/scaleph-plugin-seatunnel-native-flink/src/main/java/cn/sliew/scaleph/plugin/seatunnel/flink/SeatunnelNativeFlinkSubmitter.java
@@ -2,11 +2,13 @@ package cn.sliew.scaleph.plugin.seatunnel.flink;
 
 import cn.sliew.flinkful.cli.base.submit.PackageJarJob;
 import cn.sliew.scaleph.plugin.framework.property.PropertyDescriptor;
+import com.google.common.graph.MutableGraph;
 import org.apache.flink.configuration.Configuration;
 
 import java.net.URL;
 import java.util.Collections;
 import java.util.List;
+import java.util.Map;
 import java.util.Properties;
 
 /**
@@ -35,8 +37,9 @@ public class SeatunnelNativeFlinkSubmitter {
 
     /**
      * todo connector graph + env
+     * fixme 放在这里，不合适。。。
      */
-    public URL buildConfigFile() {
+    public URL buildConfigFile(MutableGraph<Map<String, String>> graph) {
         return null;
     }
 }
