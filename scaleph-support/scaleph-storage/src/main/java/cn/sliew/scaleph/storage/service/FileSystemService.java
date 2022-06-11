abdf297726d6196892fd7a1f73b41b4abd9eab8d
wangqi <1942460489@qq.com>
2022-06-11 08:06:30 +0800

    feature: add flink file system as storage service.

--- scaleph-support/scaleph-storage/src/main/java/cn/sliew/scaleph/storage/service/FileSystemService.java
+++ scaleph-support/scaleph-storage/src/main/java/cn/sliew/scaleph/storage/service/FileSystemService.java
@@ -1,6 +1,19 @@
 package cn.sliew.scaleph.storage.service;
 
+import org.apache.flink.core.fs.FileSystem;
+
+import java.io.IOException;
+import java.io.InputStream;
+
 public interface FileSystemService {
 
+    FileSystem getFileSystem();
 
+    InputStream get(String fileName) throws IOException;
+
+    void upload(InputStream inputStream, String fileName) throws IOException;
+
+    boolean delete(String fileName) throws IOException;
+
+    Long getFileSize(String fileName) throws IOException;
 }
--- scaleph-support/scaleph-storage/src/main/java/cn/sliew/scaleph/storage/service/impl/FileSystemServiceImpl.java
+++ scaleph-support/scaleph-storage/src/main/java/cn/sliew/scaleph/storage/service/impl/FileSystemServiceImpl.java
@@ -2,16 +2,53 @@ package cn.sliew.scaleph.storage.service.impl;
 
 import cn.sliew.scaleph.storage.service.FileSystemService;
 import lombok.extern.slf4j.Slf4j;
+import org.apache.flink.core.fs.FSDataOutputStream;
+import org.apache.flink.core.fs.FileStatus;
 import org.apache.flink.core.fs.FileSystem;
+import org.apache.flink.core.fs.Path;
+import org.apache.flink.util.IOUtils;
 import org.springframework.stereotype.Service;
+
+import javax.annotation.Resource;
+import java.io.IOException;
+import java.io.InputStream;
 
 @Slf4j
 @Service
 public class FileSystemServiceImpl implements FileSystemService {
 
+    @Resource
     private FileSystem fs;
 
-    public FileSystemServiceImpl() {
-        this.fs = fs;
+    @Override
+    public FileSystem getFileSystem() {
+        return fs;
+    }
+
+    @Override
+    public InputStream get(String fileName) throws IOException {
+        Path path = new Path(fs.getWorkingDirectory(), fileName);
+        return fs.open(path);
+    }
+
+    @Override
+    public void upload(InputStream inputStream, String fileName) throws IOException {
+        Path path = new Path(fs.getWorkingDirectory(), fileName);
+        try (final FSDataOutputStream outputStream = fs.create(path, FileSystem.WriteMode.NO_OVERWRITE)) {
+            IOUtils.copyBytes(inputStream, outputStream);
+        }
+    }
+
+    @Override
+    public boolean delete(String fileName) throws IOException {
+        Path path = new Path(fs.getWorkingDirectory(), fileName);
+        return fs.delete(path, true);
+    }
+
+    @Override
+    public Long getFileSize(String fileName) throws IOException {
+        Path path = new Path(fs.getWorkingDirectory(), fileName);
+        final FileStatus fileStatus = fs.getFileStatus(path);
+        return fileStatus.getBlockSize();
     }
 }
