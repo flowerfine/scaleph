d11b53f7ace6bf46f31abced518e0f051b8f9af0
kalencaya <1942460489@qq.com>
2022-06-18 22:10:35 +0800

    [Feature]upgrade seatunnel version to 2.1.2 (#81)
    * feature: flink cluster instance table schema
    
    * feature: upgrade seatunnel version to 2.1.2

--- tools/docker/mysql/init.d/scaleph-flink-mysql.sql
+++ tools/docker/mysql/init.d/scaleph-flink-mysql.sql
@@ -32,7 +32,7 @@ CREATE TABLE flink_deploy_config_file
     PRIMARY KEY (id)
 ) ENGINE = INNODB COMMENT = 'flink 部署配置文件';
 
-/* flink flink_cluster_config */
+/* flink 集群配置 */
 DROP TABLE IF EXISTS flink_cluster_config;
 CREATE TABLE `flink_cluster_config`
 (
@@ -51,6 +51,25 @@ CREATE TABLE `flink_cluster_config`
     `update_time`           TIMESTAMP    NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP COMMENT '修改时间',
     PRIMARY KEY (`id`)
 ) ENGINE = INNODB COMMENT = 'flink 集群配置';
+
+/* flink 集群实例 */
+DROP TABLE IF EXISTS flink_cluster_instance;
+CREATE TABLE `flink_cluster_instance`
+(
+    `id`                      BIGINT       NOT NULL AUTO_INCREMENT COMMENT '自增主键',
+    `flink_cluster_config_id` BIGINT       NOT NULL COMMENT '集群配置',
+    `name`                    VARCHAR(255) NOT NULL COMMENT '集群名称',
+    `cluster_id`              VARCHAR(64)  NOT NULL DEFAULT '' COMMENT '集群id',
+    `web_interface_url`       VARCHAR(255) NOT NULL COMMENT '集群 web-ui 链接',
+    `status`                  TINYINT      NOT NULL COMMENT '集群状态。0: 停止, 1: 运行',
+    `remark`                  VARCHAR(256)          DEFAULT NULL COMMENT '备注',
+    `creator`                 VARCHAR(32) COMMENT '创建人',
+    `create_time`             TIMESTAMP             DEFAULT CURRENT_TIMESTAMP COMMENT '创建时间',
+    `editor`                  VARCHAR(32) COMMENT '修改人',
+    `update_time`             TIMESTAMP             DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP COMMENT '修改时间',
+    PRIMARY KEY (`id`),
+    KEY idx_flink_cluster_config_id (`flink_cluster_config_id`)
+) ENGINE = INNODB COMMENT = 'flink 集群实例';
 
 /* flink artifact */
 DROP TABLE IF EXISTS flink_artifact;
