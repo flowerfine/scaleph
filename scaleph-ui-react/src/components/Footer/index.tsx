6e8f5b3edea2cb65cb779e2b9945b2d82f1058fb
maxiao <m543438924@163.com>
2022-07-06 23:04:19 +0800

    [Feature][scaleph-ui-react] change Footer

--- scaleph-ui-react/src/components/Footer/index.tsx
+++ scaleph-ui-react/src/components/Footer/index.tsx
@@ -1,14 +1,34 @@
+import { GithubOutlined } from '@ant-design/icons';
 import { DefaultFooter } from '@ant-design/pro-components';
-
 const Footer: React.FC = () => {
-  const currentYear = new Date().getFullYear();
-
+  // const currentYear = new Date().getFullYear();
   return (
     <DefaultFooter
       style={{
         background: 'none',
       }}
-      copyright={`${currentYear} Scaleph`}
+      links={[
+        {
+          key: 'Apache Seatunnel',
+          title: 'Apache Seatunnel',
+          href: 'https://seatunnel.apache.org/',
+          blankTarget: true,
+        },
+        {
+          key: 'github',
+          title: <GithubOutlined />,
+          href: 'https://github.com/flowerfine/scaleph',
+          blankTarget: true,
+        },
+        {
+          key: 'Ant Design',
+          title: 'Ant Design',
+          href: 'https://ant.design',
+          blankTarget: true,
+        },
+      ]}
+      // copyright={`${currentYear} Scaleph`}
+      copyright={false}
     />
   );
 };
