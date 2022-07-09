83f1dd0efea0abb869a1ffda67c738eb41df3c0d
wangqi <1942460489@qq.com>
2022-07-09 20:12:31 +0800

    feature: flink artifact service.

---
+++ scaleph-ui/src/app/@core/data/job.data.ts
@@ -0,0 +1,23 @@
+import { Dict, QueryParam } from './app.data';
+
+export class FlinkArtifact {
+  id?: number;
+  name?: string;
+  path?: string;
+  entryClass?: string;
+  remark?: string;
+  createTime?: Date;
+  updateTime?: Date;
+}
+
+export class FlinkArtifactParam extends QueryParam {
+  name?: string;
+}
+
+export class FlinkArtifactUploadParam {
+  name?: string;
+  entryClass?: File;
+  remark?: string;
+}
+
+
