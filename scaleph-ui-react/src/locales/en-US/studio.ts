595cf1ea2edf1cd302b3897505f0b9a0031fdd68
Dreamcreative <m543438924@163.com>
2022-08-02 19:58:09 +0800

    [Feature][scaleph-ui-react]add databoard page (#165)
    * [Feature][scaleph-ui-react] login default remember true
    
    * [Feature][scaleph-ui-react] i18n studio
    
    * [Feature][scaleph-ui-react] databoard

---
+++ scaleph-ui-react/src/locales/en-US/studio.ts
@@ -0,0 +1,14 @@
+export default {
+  'studio.databoard.projectCnt': 'Project Countes',
+  'studio.databoard.clusterCnt': 'Cluster Countes',
+  'studio.databoard.realtimeJobCnt': 'Realtime Job Countes',
+  'studio.databoard.batchJobCnt': 'Batch Job Countes',
+  'studio.databoard.batchTop100In7d': 'Batch Top100 In 7d',
+  'studio.databoard.realtimeJobRuntimeStatus': 'Realtime Job Runtime Status',
+  'studio.databoard.project': 'Project',
+  'studio.databoard.jobCode': 'Job Code',
+  'studio.databoard.duration': 'Duration',
+  'studio.databoard.startTime': 'StartTime',
+  'studio.databoard.endTime': 'EndTime',
+  'studio.databoard.jobInstanceState': 'Job Instance State',
+};
--- scaleph-ui-react/src/pages/Studio/DataBoard/index.tsx
+++ scaleph-ui-react/src/pages/Studio/DataBoard/index.tsx
@@ -1,5 +1,62 @@
+import { cluster, job } from '@/services/studio/index';
+import { Card, Col, Row, Statistic } from 'antd';
+import { useCallback, useEffect, useState } from 'react';
+import { useIntl } from 'umi';
+
 const DataBoard: React.FC = () => {
-    return (<div>DataBoard works</div>);
-}
+  const intl = useIntl();
+  const [clusterCnt, setClusterCnt] = useState(0);
+  const [batchJobCnt, setBatchJobCnt] = useState(0);
+  const [realtimeJobCnt, setRealtimeJobCnt] = useState(0);
+  useEffect(() => {
+    fetchCluster();
+    fetchJob({ jobType: 'b' }).then((d) => setBatchJobCnt(d));
+    fetchJob({ jobType: 'r' }).then((d) => setRealtimeJobCnt(d));
+  }, []);
+  // 集群数量
+  const fetchCluster = useCallback(async () => {
+    const clusterCnt = await cluster();
+    setClusterCnt(clusterCnt);
+  }, []);
+  // 任务数量
+  const fetchJob = useCallback(async (params: { jobType: string }) => {
+    const clusterCnt = await job(params);
+    return clusterCnt;
+  }, []);
+
+  return (
+    <>
+      <Row gutter={4}>
+        <Col span="8">
+          <Card>
+            <Statistic
+              title={intl.formatMessage({ id: 'studio.databoard.clusterCnt' })}
+              value={clusterCnt}
+              valueStyle={{ color: '#3f8600' }}
+            />
+          </Card>
+        </Col>
+        <Col span="8">
+          <Card>
+            <Statistic
+              title={intl.formatMessage({ id: 'studio.databoard.batchJobCnt' })}
+              value={batchJobCnt}
+              valueStyle={{ color: '#3f8600' }}
+            />
+          </Card>
+        </Col>
+        <Col span="8">
+          <Card>
+            <Statistic
+              title={intl.formatMessage({ id: 'studio.databoard.realtimeJobCnt' })}
+              value={realtimeJobCnt}
+              valueStyle={{ color: '#3f8600' }}
+            />
+          </Card>
+        </Col>
+      </Row>
+    </>
+  );
+};
 
 export default DataBoard;
