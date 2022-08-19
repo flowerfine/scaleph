0e542121e8b06f14179452735e9b0a758af0c910
gleiyu <gleiyu@sina.cn>
2022-08-19 13:19:54 +0800

    split port paramenter form host in clickhouse datasource

--- scaleph-plugins/scaleph-plugin-datasource/src/main/java/cn/sliew/scaleph/plugin/datasource/clickhouse/ClickHouseDataSourcePlugin.java
+++ scaleph-plugins/scaleph-plugin-datasource/src/main/java/cn/sliew/scaleph/plugin/datasource/clickhouse/ClickHouseDataSourcePlugin.java
@@ -1,21 +1,17 @@
 package cn.sliew.scaleph.plugin.datasource.clickhouse;
 
-import static cn.sliew.scaleph.plugin.datasource.clickhouse.ClickhouseProperties.DATABASE;
-import static cn.sliew.scaleph.plugin.datasource.jdbc.JdbcPoolProperties.DATABASE_NAME;
-import static cn.sliew.scaleph.plugin.datasource.jdbc.JdbcPoolProperties.DRIVER_CLASS_NAME_UNREQUIRED;
-import static cn.sliew.scaleph.plugin.datasource.jdbc.JdbcPoolProperties.HOST;
-import static cn.sliew.scaleph.plugin.datasource.jdbc.JdbcPoolProperties.JDBC_URL_UNREQUIRED;
-import static cn.sliew.scaleph.plugin.datasource.jdbc.JdbcPoolProperties.PASSWORD;
-import static cn.sliew.scaleph.plugin.datasource.jdbc.JdbcPoolProperties.USERNAME;
-
 import cn.sliew.scaleph.common.enums.DataSourceTypeEnum;
 import cn.sliew.scaleph.plugin.datasource.jdbc.JDBCDataSourcePlugin;
 import cn.sliew.scaleph.plugin.framework.core.PluginInfo;
 import cn.sliew.scaleph.plugin.framework.property.PropertyContext;
 import cn.sliew.scaleph.plugin.framework.property.PropertyDescriptor;
+
 import java.util.ArrayList;
 import java.util.Collections;
 import java.util.List;
+
+import static cn.sliew.scaleph.plugin.datasource.clickhouse.ClickhouseProperties.DATABASE;
+import static cn.sliew.scaleph.plugin.datasource.jdbc.JdbcPoolProperties.*;
 
 public class ClickHouseDataSourcePlugin extends JDBCDataSourcePlugin {
 
@@ -42,7 +38,7 @@ public class ClickHouseDataSourcePlugin extends JDBCDataSourcePlugin {
 
     @Override
     public String getJdbcUrl() {
-        return "jdbc:clickhouse://" + properties.getString(HOST)+ "/" + properties.getString(DATABASE_NAME) + "?" + getAdditionalProps();
+        return "jdbc:clickhouse://" + properties.getString(HOST) + ":" + properties.get(PORT) + "/" + properties.getString(DATABASE_NAME) + "?" + getAdditionalProps();
     }
 
     @Override
