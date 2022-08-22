50a2c61e6ebe1e6507f614b7611560fe0ac6852a
gleiyu <gleiyu@sina.cn>
2022-08-23 01:58:41 +0800

    user center security

--- scaleph-ui-react/src/pages/User/components/Security/index.tsx
+++ scaleph-ui-react/src/pages/User/components/Security/index.tsx
@@ -1,5 +1,80 @@
+import { SecUser } from '@/services/admin/typings';
+import { getUserInfo } from '@/services/admin/user.service';
+import { Button, List, Typography } from 'antd';
+import { useEffect, useState } from 'react';
+import { useIntl } from 'umi';
+
 const Security: React.FC = () => {
-    return (<div>Security works</div>);
-}
+  const intl = useIntl();
+  const [user, setUser] = useState<SecUser>();
+
+  const refreshUserInfo = () => {
+    getUserInfo().then((resp) => {
+      setUser(resp);
+    });
+  };
+
+  useEffect(() => {
+    refreshUserInfo();
+  }, []);
+
+  return (
+    <div>
+      <List itemLayout="horizontal">
+        <List.Item
+          extra={
+            <Button
+              type="link"
+              onClick={() => {
+                alert('edit password');
+              }}
+            >
+              {intl.formatMessage({ id: 'pages.admin.usercenter.security.password.edit' })}
+            </Button>
+          }
+        >
+          <List.Item.Meta
+            title={
+              <Typography.Text strong>
+                {intl.formatMessage({ id: 'pages.admin.usercenter.security.password' })}
+              </Typography.Text>
+            }
+            description={
+              <Typography.Text type="secondary">
+                {intl.formatMessage({ id: 'pages.admin.usercenter.security.password.level' })}
+              </Typography.Text>
+            }
+          ></List.Item.Meta>
+        </List.Item>
+        <List.Item
+          extra={
+            <Button
+              type="link"
+              onClick={() => {
+                alert('bind email');
+              }}
+            >
+              {intl.formatMessage({ id: 'pages.admin.usercenter.security.email.bind' })}
+            </Button>
+          }
+        >
+          <List.Item.Meta
+            title={
+              <Typography.Text strong>
+                {intl.formatMessage({ id: 'pages.admin.usercenter.security.email' })}
+              </Typography.Text>
+            }
+            description={
+              <Typography.Text type="secondary">
+                {user?.userStatus?.label}
+                {user?.userStatus?.value == '11' ? '\t' + user.email : ''}
+              </Typography.Text>
+            }
+          ></List.Item.Meta>
+        </List.Item>
+      </List>
+    </div>
+  );
+};
 
 export default Security;
