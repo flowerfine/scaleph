e8d0082c3ca34c4894f93a2ee23efcb673664843
gleiyu <gleiyu@sina.cn>
2022-08-22 21:45:21 +0800

    user center profile

--- scaleph-ui-react/src/pages/User/components/Profile/index.tsx
+++ scaleph-ui-react/src/pages/User/components/Profile/index.tsx
@@ -1,241 +1,266 @@
 import { Dict } from '@/app.d';
 import { DICT_TYPE } from '@/constant';
 import { listDictDataByType } from '@/services/admin/dictData.service';
-import { isEmailExists, isUserExists, } from '@/services/admin/user.service';
-import { Button, Card, Col, DatePicker, Form, Input, Row, Select } from 'antd';
+import { SecUser } from '@/services/admin/typings';
+import { getUserInfo, updateUser } from '@/services/admin/user.service';
+import { Button, Card, Col, DatePicker, Form, Input, message, Row, Select } from 'antd';
 import moment from 'moment';
-import { useEffect, useState } from 'react';
+import { useLayoutEffect, useState } from 'react';
 import { useIntl } from 'umi';
 
 const Profile: React.FC = () => {
-    const intl = useIntl();
-    const [form] = Form.useForm();
-    const [genderList, setGenderList] = useState<Dict[]>([]);
-    const [nationList, setNationList] = useState<Dict[]>([]);
-    const [idCardTypeList, setIdCardTypeList] = useState<Dict[]>([]);
-    useEffect(() => {
-        listDictDataByType(DICT_TYPE.idCardType).then((d) => {
-            setIdCardTypeList(d);
-        });
-        listDictDataByType(DICT_TYPE.gender).then((d) => {
-            setGenderList(d);
-        });
-        listDictDataByType(DICT_TYPE.nation).then((d) => {
-            setNationList(d);
-        });
-    }, []);
+  const intl = useIntl();
+  const [form] = Form.useForm();
+  const [genderList, setGenderList] = useState<Dict[]>([]);
+  const [nationList, setNationList] = useState<Dict[]>([]);
+  const [idCardTypeList, setIdCardTypeList] = useState<Dict[]>([]);
+  useLayoutEffect(() => {
+    listDictDataByType(DICT_TYPE.idCardType).then((d) => {
+      setIdCardTypeList(d);
+    });
+    listDictDataByType(DICT_TYPE.gender).then((d) => {
+      setGenderList(d);
+    });
+    listDictDataByType(DICT_TYPE.nation).then((d) => {
+      setNationList(d);
+    });
+    refreshUserInfo();
+  }, []);
 
-    return (
-        <Card
-            style={{ width: '70%' }}
-            bordered={false}
-        >
-            <Form
-                form={form}
-                layout="vertical"
-            // initialValues={{
-            //   id: data.id,
-            //   userName: data.userName,
-            //   email: data.email,
-            //   nickName: data.nickName,
-            //   realName: data.realName,
-            //   mobilePhone: data.mobilePhone,
-            //   gender: data.gender?.value,
-            //   idCardType: data.idCardType?.value,
-            //   idCardNo: data.idCardNo,
-            //   nation: data.nation?.value,
-            //   birthday:  moment(data?.birthday, 'YYYY-MM-DD') ,
-            //   qq: data.qq,
-            //   wechat: data.wechat,
-            //   summary: data.summary,
-            // }}
+  const refreshUserInfo = () => {
+    getUserInfo().then((resp) => {
+      form.setFieldsValue({
+        id: resp.id,
+        userName: resp.userName,
+        email: resp.email,
+        nickName: resp.nickName,
+        realName: resp.realName,
+        mobilePhone: resp.mobilePhone,
+        gender: resp.gender?.value,
+        idCardType: resp.idCardType?.value,
+        idCardNo: resp.idCardNo,
+        nation: resp.nation?.value,
+        birthday: moment(resp?.birthday, 'YYYY-MM-DD HH:mm:ss'),
+        qq: resp.qq,
+        wechat: resp.wechat,
+        summary: resp.summary,
+      });
+    });
+  };
+
+  return (
+    <Card style={{ width: '70%' }} bordered={false}>
+      <Form
+        form={form}
+        layout="vertical"
+        onFinish={() => {
+          form.validateFields().then((values) => {
+            let user: SecUser = {
+              id: values.id,
+              userName: values.userName,
+              email: values.email,
+              nickName: values.nickName,
+              realName: values.realName,
+              mobilePhone: values.mobilePhone,
+              gender: { value: values.gender },
+              idCardType: { value: values.idCardType },
+              idCardNo: values.idCardNo,
+              nation: { value: values.nation },
+              birthday: values.birthday?.valueOf(),
+              qq: values.qq,
+              wechat: values.wechat,
+              summary: values.summary,
+            };
+            updateUser({ ...user }).then((d) => {
+              if (d.success) {
+                message.success(intl.formatMessage({ id: 'app.common.operate.edit.success' }));
+                refreshUserInfo();
+              }
+            });
+          });
+        }}
+      >
+        <Form.Item name="id" hidden>
+          <Input></Input>
+        </Form.Item>
+        <Row gutter={[12, 12]}>
+          <Col span={12}>
+            <Form.Item
+              name="userName"
+              label={intl.formatMessage({ id: 'pages.admin.user.userName' })}
+              rules={[
+                { required: true },
+                { max: 30 },
+                { min: 5 },
+                {
+                  pattern: /^[a-zA-Z0-9_]+$/,
+                  message: intl.formatMessage({ id: 'app.common.validate.characterWord' }),
+                },
+              ]}
             >
-                <Form.Item name="id" hidden>
-                    <Input></Input>
-                </Form.Item>
-                <Row gutter={[12, 12]}>
-                    <Col span={12}>
-                        <Form.Item
-                            name="userName"
-                            label={intl.formatMessage({ id: 'pages.admin.user.userName' })}
-                            rules={[
-                                { required: true },
-                                { max: 30 },
-                                { min: 5 },
-                                {
-                                    pattern: /^[a-zA-Z0-9_]+$/,
-                                    message: intl.formatMessage({ id: 'app.common.validate.characterWord' }),
-                                },
-                            ]}
-                        >
-                            <Input disabled={true}></Input>
-                        </Form.Item>
-                    </Col>
-                    <Col span={12}>
-                        {' '}
-                        <Form.Item
-                            name="email"
-                            label={intl.formatMessage({ id: 'pages.admin.user.email' })}
-                            rules={[
-                                { required: true },
-                                { max: 100 },
-                                { type: 'email' },
-                            ]}
-                        >
-                            <Input disabled={true}></Input>
-                        </Form.Item>
-                    </Col>
-                    <Col span={12}>
-                        <Form.Item
-                            name="nickName"
-                            label={intl.formatMessage({ id: 'pages.admin.user.nickName' })}
-                            rules={[{ max: 50 }]}
-                        >
-                            <Input></Input>
-                        </Form.Item>
-                    </Col>
-                    <Col span={12}>
-                        <Form.Item
-                            name="realName"
-                            label={intl.formatMessage({ id: 'pages.admin.user.realName' })}
-                            rules={[{ max: 30 }]}
-                        >
-                            <Input></Input>
-                        </Form.Item>
-                    </Col>
-                    <Col span={12}>
-                        <Form.Item
-                            name="mobilePhone"
-                            label={intl.formatMessage({ id: 'pages.admin.user.mobilePhone' })}
-                            rules={[{ max: 11 }]}
-                        >
-                            <Input></Input>
-                        </Form.Item>
-                    </Col>
-                    <Col span={12}>
-                        <Form.Item name="gender" label={intl.formatMessage({ id: 'pages.admin.user.gender' })}>
-                            <Select
-                                showSearch={true}
-                                allowClear={true}
-                                optionFilterProp="label"
-                                filterOption={(input, option) =>
-                                    (option!.children as unknown as string)
-                                        .toLowerCase()
-                                        .includes(input.toLowerCase())
-                                }
-                            >
-                                {genderList.map((item) => {
-                                    return (
-                                        <Select.Option key={item.value} value={item.value}>
-                                            {item.label}
-                                        </Select.Option>
-                                    );
-                                })}
-                            </Select>
-                        </Form.Item>
-                    </Col>
-                    <Col span={12}>
-                        <Form.Item
-                            name="idCardType"
-                            label={intl.formatMessage({ id: 'pages.admin.user.idCardType' })}
-                        >
-                            <Select
-                                showSearch={true}
-                                allowClear={true}
-                                optionFilterProp="label"
-                                filterOption={(input, option) =>
-                                    (option!.children as unknown as string)
-                                        .toLowerCase()
-                                        .includes(input.toLowerCase())
-                                }
-                            >
-                                {idCardTypeList.map((item) => {
-                                    return (
-                                        <Select.Option key={item.value} value={item.value}>
-                                            {item.label}
-                                        </Select.Option>
-                                    );
-                                })}
-                            </Select>
-                        </Form.Item>
-                    </Col>
-                    <Col span={12}>
-                        <Form.Item
-                            name="idCardNo"
-                            label={intl.formatMessage({ id: 'pages.admin.user.idCardNo' })}
-                            rules={[{ max: 18 }]}
-                        >
-                            <Input></Input>
-                        </Form.Item>
-                    </Col>
-                    <Col span={12}>
-                        <Form.Item name="nation" label={intl.formatMessage({ id: 'pages.admin.user.nation' })}>
-                            <Select
-                                showSearch={true}
-                                allowClear={true}
-                                optionFilterProp="label"
-                                filterOption={(input, option) =>
-                                    (option!.children as unknown as string)
-                                        .toLowerCase()
-                                        .includes(input.toLowerCase())
-                                }
-                            >
-                                {nationList.map((item) => {
-                                    return (
-                                        <Select.Option key={item.value} value={item.value}>
-                                            {item.label}
-                                        </Select.Option>
-                                    );
-                                })}
-                            </Select>
-                        </Form.Item>
-                    </Col>
-                    <Col span={12}>
-                        <Form.Item
-                            name="birthday"
-                            label={intl.formatMessage({ id: 'pages.admin.user.birthday' })}
-                            rules={[{ type: 'date' }]}
-                        >
-                            <DatePicker picker="date" style={{ width: '100%' }} allowClear={true}></DatePicker>
-                        </Form.Item>
-                    </Col>
-                    <Col span={12}>
-                        <Form.Item
-                            name="qq"
-                            label={intl.formatMessage({ id: 'pages.admin.user.qq' })}
-                            rules={[{ max: 15 }]}
-                        >
-                            <Input></Input>
-                        </Form.Item>
-                    </Col>
-                    <Col span={12}>
-                        <Form.Item
-                            name="wechat"
-                            label={intl.formatMessage({ id: 'pages.admin.user.wechat' })}
-                            rules={[{ max: 50 }]}
-                        >
-                            <Input></Input>
-                        </Form.Item>
-                    </Col>
-                    <Col span={24}>
-                        <Form.Item
-                            name="summary"
-                            label={intl.formatMessage({ id: 'pages.admin.user.summary' })}
-                            rules={[{ max: 500 }]}
-                        >
-                            <Input.TextArea></Input.TextArea>
-                        </Form.Item>
-                        <Form.Item>
-                            <Button type="primary" htmlType="submit">
-                                {intl.formatMessage({ id: 'app.common.operate.confirm.label' })}
-                            </Button>
-                        </Form.Item>
-                    </Col>
-
-                </Row>
-            </Form>
-        </Card>
-    );
-}
+              <Input disabled={true}></Input>
+            </Form.Item>
+          </Col>
+          <Col span={12}>
+            {' '}
+            <Form.Item
+              name="email"
+              label={intl.formatMessage({ id: 'pages.admin.user.email' })}
+              rules={[{ required: true }, { max: 100 }, { type: 'email' }]}
+            >
+              <Input disabled={true}></Input>
+            </Form.Item>
+          </Col>
+          <Col span={12}>
+            <Form.Item
+              name="nickName"
+              label={intl.formatMessage({ id: 'pages.admin.user.nickName' })}
+              rules={[{ max: 50 }]}
+            >
+              <Input></Input>
+            </Form.Item>
+          </Col>
+          <Col span={12}>
+            <Form.Item
+              name="realName"
+              label={intl.formatMessage({ id: 'pages.admin.user.realName' })}
+              rules={[{ max: 30 }]}
+            >
+              <Input></Input>
+            </Form.Item>
+          </Col>
+          <Col span={12}>
+            <Form.Item
+              name="mobilePhone"
+              label={intl.formatMessage({ id: 'pages.admin.user.mobilePhone' })}
+              rules={[{ max: 11 }]}
+            >
+              <Input></Input>
+            </Form.Item>
+          </Col>
+          <Col span={12}>
+            <Form.Item name="gender" label={intl.formatMessage({ id: 'pages.admin.user.gender' })}>
+              <Select
+                showSearch={true}
+                allowClear={true}
+                optionFilterProp="label"
+                filterOption={(input, option) =>
+                  (option!.children as unknown as string)
+                    .toLowerCase()
+                    .includes(input.toLowerCase())
+                }
+              >
+                {genderList.map((item) => {
+                  return (
+                    <Select.Option key={item.value} value={item.value}>
+                      {item.label}
+                    </Select.Option>
+                  );
+                })}
+              </Select>
+            </Form.Item>
+          </Col>
+          <Col span={12}>
+            <Form.Item
+              name="idCardType"
+              label={intl.formatMessage({ id: 'pages.admin.user.idCardType' })}
+            >
+              <Select
+                showSearch={true}
+                allowClear={true}
+                optionFilterProp="label"
+                filterOption={(input, option) =>
+                  (option!.children as unknown as string)
+                    .toLowerCase()
+                    .includes(input.toLowerCase())
+                }
+              >
+                {idCardTypeList.map((item) => {
+                  return (
+                    <Select.Option key={item.value} value={item.value}>
+                      {item.label}
+                    </Select.Option>
+                  );
+                })}
+              </Select>
+            </Form.Item>
+          </Col>
+          <Col span={12}>
+            <Form.Item
+              name="idCardNo"
+              label={intl.formatMessage({ id: 'pages.admin.user.idCardNo' })}
+              rules={[{ max: 18 }]}
+            >
+              <Input></Input>
+            </Form.Item>
+          </Col>
+          <Col span={12}>
+            <Form.Item name="nation" label={intl.formatMessage({ id: 'pages.admin.user.nation' })}>
+              <Select
+                showSearch={true}
+                allowClear={true}
+                optionFilterProp="label"
+                filterOption={(input, option) =>
+                  (option!.children as unknown as string)
+                    .toLowerCase()
+                    .includes(input.toLowerCase())
+                }
+              >
+                {nationList.map((item) => {
+                  return (
+                    <Select.Option key={item.value} value={item.value}>
+                      {item.label}
+                    </Select.Option>
+                  );
+                })}
+              </Select>
+            </Form.Item>
+          </Col>
+          <Col span={12}>
+            <Form.Item
+              name="birthday"
+              label={intl.formatMessage({ id: 'pages.admin.user.birthday' })}
+              rules={[{ type: 'date' }]}
+            >
+              <DatePicker picker="date" style={{ width: '100%' }} allowClear={false}></DatePicker>
+            </Form.Item>
+          </Col>
+          <Col span={12}>
+            <Form.Item
+              name="qq"
+              label={intl.formatMessage({ id: 'pages.admin.user.qq' })}
+              rules={[{ max: 15 }]}
+            >
+              <Input></Input>
+            </Form.Item>
+          </Col>
+          <Col span={12}>
+            <Form.Item
+              name="wechat"
+              label={intl.formatMessage({ id: 'pages.admin.user.wechat' })}
+              rules={[{ max: 50 }]}
+            >
+              <Input></Input>
+            </Form.Item>
+          </Col>
+          <Col span={24}>
+            <Form.Item
+              name="summary"
+              label={intl.formatMessage({ id: 'pages.admin.user.summary' })}
+              rules={[{ max: 500 }]}
+            >
+              <Input.TextArea></Input.TextArea>
+            </Form.Item>
+            <Form.Item>
+              <Button type="primary" htmlType="submit">
+                {intl.formatMessage({ id: 'app.common.operate.confirm.label' })}
+              </Button>
+            </Form.Item>
+          </Col>
+        </Row>
+      </Form>
+    </Card>
+  );
+};
 
 export default Profile;
