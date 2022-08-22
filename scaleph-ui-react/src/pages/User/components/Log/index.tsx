5f3171c8937f6b6dcf65b56e2a233856773e3268
gleiyu <gleiyu@sina.cn>
2022-08-22 22:56:06 +0800

    user center login log

--- scaleph-ui-react/src/pages/User/components/Log/index.tsx
+++ scaleph-ui-react/src/pages/User/components/Log/index.tsx
@@ -1,5 +1,76 @@
+import { listLoginLogByPage } from '@/services/admin/log.service';
+import { LogLogin } from '@/services/admin/typings';
+import { List, Pagination, Typography } from 'antd';
+import { useEffect, useState } from 'react';
+import { useIntl } from 'umi';
+
 const Log: React.FC = () => {
-    return (<div>Log works</div>);
-}
+  const intl = useIntl();
+  const [logs, setLogs] = useState<LogLogin[]>([]);
+  const [pageInfo, setPageInfo] = useState<{ pageSize: number; current: number }>({
+    pageSize: 2,
+    current: 1,
+  });
+  const [total, setTotal] = useState<number>(0);
+
+  useEffect(() => {
+    refreshLogs(pageInfo.pageSize, pageInfo.current);
+  }, []);
+
+  const refreshLogs = (pageSize: number, current: number) => {
+    listLoginLogByPage({ pageSize, current }).then((resp) => {
+      setLogs(resp.data);
+      setTotal(resp.total);
+    });
+  };
+
+  return (
+    <div>
+      <List
+        itemLayout="horizontal"
+        dataSource={logs}
+        renderItem={(item) => (
+          <List.Item>
+            <List.Item.Meta
+              title={
+                <Typography.Text strong>
+                  {item.loginType?.label}&nbsp;&nbsp; {item.loginTime}
+                </Typography.Text>
+              }
+              description={
+                <Typography.Text type="secondary">
+                  {intl.formatMessage({ id: 'pages.admin.usercenter.log.ipAddress' }) +
+                    item.ipAddress}
+                  &nbsp;&nbsp;
+                  {intl.formatMessage({ id: 'pages.admin.usercenter.log.clientInfo' }) +
+                    item.clientInfo}
+                  &nbsp;&nbsp;
+                  {intl.formatMessage({ id: 'pages.admin.usercenter.log.os' }) + item.osInfo}
+                  &nbsp;&nbsp;
+                  {intl.formatMessage({ id: 'pages.admin.usercenter.log.browser' }) +
+                    item.browserInfo}
+                  &nbsp;&nbsp;
+                </Typography.Text>
+              }
+            ></List.Item.Meta>
+          </List.Item>
+        )}
+      ></List>
+      <Pagination
+        size="small"
+        showSizeChanger
+        showQuickJumper
+        onChange={(page, pageSize) => {
+          setPageInfo({ pageSize: pageSize, current: page });
+          refreshLogs(pageSize, page);
+        }}
+        total={total}
+        pageSize={pageInfo.pageSize}
+        current={pageInfo.current}
+        style={{ paddingTop: 12 }}
+      ></Pagination>
+    </div>
+  );
+};
 
 export default Log;
