a547fc5ca4ce01d2a950509bd086fad9b1ad8ca6
wangqi <1942460489@qq.com>
2022-08-23 08:36:32 +0800

    feature: add akka workflow engine.

---
+++ scaleph-workflow/scaleph-workflow-api/src/main/java/cn/sliew/scaleph/workflow/engine/workflow/SequentialFlow.java
@@ -0,0 +1,59 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements.  See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership.  The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License.  You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package cn.sliew.scaleph.workflow.engine.workflow;
+
+import akka.Done;
+import akka.actor.typed.ActorSystem;
+import akka.stream.javadsl.Sink;
+import akka.stream.javadsl.Source;
+import cn.sliew.milky.common.chain.ContextMap;
+import cn.sliew.milky.common.filter.ActionListener;
+import cn.sliew.scaleph.workflow.engine.action.Action;
+import cn.sliew.scaleph.workflow.engine.action.ActionResult;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.concurrent.CompletionStage;
+
+public class SequentialFlow extends AbstractWorkflow {
+
+    private final ActorSystem actorSystem;
+    private final List<Action> actions = new ArrayList<>();
+
+    public SequentialFlow(String name, ActorSystem actorSystem, List<Action> actions) {
+        super(name);
+        this.actorSystem = actorSystem;
+        this.actions.addAll(actions);
+    }
+
+    @Override
+    public void execute(ContextMap<String, Object> context, ActionListener<ActionResult> listener) {
+        final CompletionStage<Done> future = Source.from(actions).runWith(doExecute(context, listener), actorSystem);
+        future.whenComplete((done, throwable) -> {
+            if (throwable != null) {
+                listener.onFailure(new Exception(throwable));
+            }
+        });
+    }
+
+    private Sink<Action, CompletionStage<Done>> doExecute(ContextMap<String, Object> context,
+                                                          ActionListener<ActionResult> listener) {
+        return Sink.foreach(action -> action.execute(context, listener));
+    }
+}
