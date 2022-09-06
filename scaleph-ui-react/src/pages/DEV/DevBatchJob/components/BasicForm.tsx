a29aa9b9d46abdae56f4486ef9256e49fee1871e
wangqi <1942460489@qq.com>
2022-09-06 23:21:19 +0800

    feature: flink cluster config web.

--- scaleph-ui-react/src/pages/DEV/DevBatchJob/components/BasicForm.tsx
+++ scaleph-ui-react/src/pages/DEV/DevBatchJob/components/BasicForm.tsx
@@ -1,5 +1,5 @@
 import {Card, Form, Select} from "antd";
-import {ProForm, ProFormSelect, ProFormText} from "@ant-design/pro-components";
+import {ProForm, ProFormSelect, ProFormText, RequestOptionsType} from "@ant-design/pro-components";
 import {useIntl} from "@@/exports";
 import {useEffect, useState} from "react";
 import {Dict} from '@/app.d';
@@ -11,22 +11,12 @@ import {list} from "@/services/resource/resource.service";
 const BasicForm: React.FC = () => {
   const intl = useIntl();
   const [form] = Form.useForm();
-  const [flinkVersionList, setFlinkVersionList] = useState<Dict[]>([]);
-  const [resourceProviderList, setResourceProviderList] = useState<Dict[]>([]);
-  const [deployModeList, setDeployModeList] = useState<Dict[]>([]);
   const [flinkReleaseList, setFlinkReleaseList] = useState<Resource[]>([]);
   const [clusterCredentialList, setClusterCredentialList] = useState<Resource[]>([]);
 
+  // [key:string]:any
+
   useEffect(() => {
-    listDictDataByType(DICT_TYPE.flinkVersion).then((d) => {
-      setFlinkVersionList(d);
-    });
-    listDictDataByType(DICT_TYPE.flinkResourceProvider).then((d) => {
-      setResourceProviderList(d);
-    });
-    listDictDataByType(DICT_TYPE.flinkDeploymentMode).then((d) => {
-      setDeployModeList(d);
-    });
     const flinkReleaseParam: ResourceListParam = {
       resourceType: RESOURCE_TYPE.flinkRelease,
     }
@@ -46,9 +36,6 @@ const BasicForm: React.FC = () => {
       title={"Basic"}
       collapsible={true}
     >
-      <Card title="仓库管理" bordered={false}>
-
-      </Card>
       <ProFormText
         name="name"
         label={intl.formatMessage({id: 'pages.dev.clusterConfig.name'})}
@@ -68,113 +55,52 @@ const BasicForm: React.FC = () => {
         rules={[{required: true}]}
         showSearch={true}
         allowClear={true}
+        request={() => listDictDataByType(DICT_TYPE.flinkVersion)}
       />
-      <Form.Item
+      <ProFormSelect
         name="flinkRelease"
         label={intl.formatMessage({id: 'pages.dev.clusterConfig.flinkRelease'})}
+        colProps={{xl: 8, md: 12}}
         rules={[{required: true}]}
-      >
-        <Select
-          showSearch={true}
-          allowClear={true}
-          optionFilterProp="label"
-          filterOption={(input, option) =>
-            (option!.children as unknown as string)
-              .toLowerCase()
-              .includes(input.toLowerCase())
+        showSearch={true}
+        allowClear={true}
+        dependencies={['flinkVersion']}
+        request={(params) => {
+          const flinkReleaseParam: ResourceListParam = {
+            resourceType: RESOURCE_TYPE.flinkRelease,
+            label: params.flinkVersion
           }
-        >
-          {flinkReleaseList.map((item) => {
-            return (
-              <Select.Option key={item.id} value={item.id}>
-                {item.name}
-              </Select.Option>
-            );
-          })}
-        </Select>
-      </Form.Item>
-      <Form.Item
+          return list(flinkReleaseParam).then((response) => response.records)
+        }}
+      />
+      <ProFormSelect
         name="deployMode"
         label={intl.formatMessage({id: 'pages.dev.clusterConfig.deployMode'})}
+        colProps={{xl: 8, md: 12}}
         rules={[{required: true}]}
-      >
-        <Select
-          showSearch={true}
-          allowClear={true}
-          optionFilterProp="label"
-          filterOption={(input, option) =>
-            (option!.children as unknown as string)
-              .toLowerCase()
-              .includes(input.toLowerCase())
-          }
-        >
-          {deployModeList.map((item) => {
-            return (
-              <Select.Option key={item.value} value={item.value}>
-                {item.label}
-              </Select.Option>
-            );
-          })}
-        </Select>
-      </Form.Item>
-      <Form.Item
+        showSearch={true}
+        allowClear={true}
+        request={() => listDictDataByType(DICT_TYPE.flinkDeploymentMode)}
+      />
+      <ProFormSelect
         name="resourceProvider"
         label={intl.formatMessage({id: 'pages.dev.clusterConfig.resourceProvider'})}
+        colProps={{xl: 8, md: 12}}
         rules={[{required: true}]}
-      >
-        <Select
-          showSearch={true}
-          allowClear={true}
-          optionFilterProp="label"
-          filterOption={(input, option) =>
-            (option!.children as unknown as string)
-              .toLowerCase()
-              .includes(input.toLowerCase())
-          }
-          onChange={(value, option) => {
-            const param: ResourceListParam = {
-              resourceType: RESOURCE_TYPE.clusterCredential,
-              label: value,
-            }
-            list(param).then((d) => {
-              setClusterCredentialList(d.records);
-            });
-            form.setFieldValue('clusterCredential', null)
-          }}
-        >
-          {resourceProviderList.map((item) => {
-            return (
-              <Select.Option key={item.value} value={item.value}>
-                {item.label}
-              </Select.Option>
-            );
-          })}
-        </Select>
-      </Form.Item>
-      <Form.Item
+        showSearch={true}
+        allowClear={true}
+        request={() => listDictDataByType(DICT_TYPE.flinkResourceProvider)}
+      />
+
+      <ProFormSelect
         name="clusterCredential"
         label={intl.formatMessage({id: 'pages.dev.clusterConfig.clusterCredential'})}
+        colProps={{xl: 8, md: 12}}
         rules={[{required: true}]}
-      >
-        <Select
-          showSearch={true}
-          allowClear={true}
-          optionFilterProp="label"
-          filterOption={(input, option) =>
-            (option!.children as unknown as string)
-              .toLowerCase()
-              .includes(input.toLowerCase())
-          }
-        >
-          {clusterCredentialList.map((item) => {
-            return (
-              <Select.Option key={item.id} value={item.id}>
-                {item.name}
-              </Select.Option>
-            );
-          })}
-        </Select>
-      </Form.Item>
+        showSearch={true}
+        allowClear={true}
+      />
+
     </ProForm.Group>
   </div>)
 }
