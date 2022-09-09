4174dbd81934c25b22e40cf4de26d16d253c085a
wangqi <1942460489@qq.com>
2022-09-09 08:17:16 +0800

    feature: imports optimized

--- scaleph-ui-react/src/services/resource/clusterCredential.service.ts
+++ scaleph-ui-react/src/services/resource/clusterCredential.service.ts
@@ -7,7 +7,6 @@ import {
 import {request} from "@@/exports";
 import {PageResponse, ResponseBody} from "@/app.d";
 import {USER_AUTH} from "@/constant";
-import {UploadFile} from "antd";
 
 const url: string = '/api/resource/cluster-credential';
 
