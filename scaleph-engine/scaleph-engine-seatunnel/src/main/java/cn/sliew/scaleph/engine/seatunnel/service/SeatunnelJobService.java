4deb5abfb899afc2190866ae129529fa0b07a31b
wangqi <1942460489@qq.com>
2023-01-06 21:50:53 +0800

    fix: remove unused source code

--- scaleph-engine/scaleph-engine-seatunnel/src/main/java/cn/sliew/scaleph/engine/seatunnel/service/SeatunnelJobService.java
+++ scaleph-engine/scaleph-engine-seatunnel/src/main/java/cn/sliew/scaleph/engine/seatunnel/service/SeatunnelJobService.java
@@ -18,43 +18,14 @@
 
 package cn.sliew.scaleph.engine.seatunnel.service;
 
-import cn.sliew.scaleph.engine.seatunnel.service.dto.WsDiJobDTO;
-import cn.sliew.scaleph.engine.seatunnel.service.vo.DiJobRunVO;
 import cn.sliew.scaleph.engine.seatunnel.service.dto.DagPanelDTO;
 import cn.sliew.scaleph.plugin.framework.exception.PluginException;
-import org.apache.flink.configuration.Configuration;
 
-import java.io.File;
-import java.io.IOException;
-import java.nio.file.Path;
 import java.util.List;
-import java.util.Map;
 
-@Deprecated
 public interface SeatunnelJobService {
 
     String preview(Long jobId) throws Exception;
-
-    void run(DiJobRunVO jobRunParam) throws Exception;
-
-    void submit(WsDiJobDTO wsDiJobDTO) throws Exception;
-
-    void schedule(WsDiJobDTO wsDiJobDTO) throws Exception;
-
-    void stop(Long jobId) throws Exception;
-
-    void cancel(WsDiJobDTO wsDiJobDTO) throws Exception;
-
-    void unschedule(WsDiJobDTO wsDiJobDTO) throws Exception;
-
-    // todo remove
-    Path buildConfFile(WsDiJobDTO wsDiJobDTO, Path projectPath) throws Exception;
-
-    Path getSeatunnelJar() throws IOException;
-
-    Configuration buildConfiguration(WsDiJobDTO job, Path seatunnelJarPath,
-                                     Map<String, String> clusterConf,
-                                     File projectPath) throws IOException;
 
     List<DagPanelDTO> loadDndPanelInfo() throws PluginException;
 }
