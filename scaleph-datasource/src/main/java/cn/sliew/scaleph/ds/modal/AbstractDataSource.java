a1419262bea6d32005373e8927e6482774924e1b
wangqi <1942460489@qq.com>
2023-01-08 17:25:44 +0800

    feature: add doris datasource

--- scaleph-datasource/src/main/java/cn/sliew/scaleph/ds/modal/AbstractDataSource.java
+++ scaleph-datasource/src/main/java/cn/sliew/scaleph/ds/modal/AbstractDataSource.java
@@ -29,10 +29,7 @@ import cn.sliew.scaleph.ds.modal.nosql.CassandraDataSource;
 import cn.sliew.scaleph.ds.modal.nosql.ElasticsearchDataSource;
 import cn.sliew.scaleph.ds.modal.nosql.MongoDBDataSource;
 import cn.sliew.scaleph.ds.modal.nosql.RedisDataSource;
-import cn.sliew.scaleph.ds.modal.olap.ClickHouseDataSource;
-import cn.sliew.scaleph.ds.modal.olap.HiveDataSource;
-import cn.sliew.scaleph.ds.modal.olap.KuduDataSource;
-import cn.sliew.scaleph.ds.modal.olap.StarRocksDataSource;
+import cn.sliew.scaleph.ds.modal.olap.*;
 import cn.sliew.scaleph.ds.service.dto.DsInfoDTO;
 import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 import com.fasterxml.jackson.annotation.JsonSubTypes;
@@ -78,6 +75,7 @@ import java.util.List;
 
         @JsonSubTypes.Type(name = "ClickHouse", value = ClickHouseDataSource.class),
         @JsonSubTypes.Type(name = "Kudu", value = KuduDataSource.class),
+        @JsonSubTypes.Type(name = "Doris", value = DorisDataSource.class),
         @JsonSubTypes.Type(name = "StarRocks", value = StarRocksDataSource.class),
 
         @JsonSubTypes.Type(name = "IoTDB", value = IoTDBDataSource.class),
