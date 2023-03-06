3c281984304024d1460ac0cafdd7e43f3165c2d4
wangqi <1942460489@qq.com>
2023-03-06 08:25:51 +0800

    feature: refactor flink kubernetes session cluster

--- tools/docker/mysql/init.d/scaleph-ws-mysql.sql
+++ tools/docker/mysql/init.d/scaleph-ws-mysql.sql
@@ -164,7 +164,7 @@ CREATE TABLE ws_flink_cluster_instance
     update_time             timestamp default current_timestamp on update current_timestamp comment '修改时间',
     PRIMARY KEY (id),
     unique key (project_id, flink_cluster_config_id, name),
-    KEY                     idx_name ( name)
+    KEY idx_name (name)
 ) ENGINE = INNODB COMMENT = 'flink cluster instance';
 
 drop table if exists ws_flink_artifact;
@@ -220,10 +220,10 @@ create table ws_flink_job
     update_time               timestamp default current_timestamp on update current_timestamp comment '修改时间',
     primary key (id),
     unique key idx_code (code),
-    key                       idx_name ( type, name),
-    key                       idx_flink_artifact ( type, flink_artifact_id),
-    key                       idx_flink_cluster_config (flink_cluster_config_id),
-    key                       idx_flink_cluster_instance (flink_cluster_instance_id)
+    key idx_name (type, name),
+    key idx_flink_artifact (type, flink_artifact_id),
+    key idx_flink_cluster_config (flink_cluster_config_id),
+    key idx_flink_cluster_instance (flink_cluster_instance_id)
 ) engine = innodb comment ='flink作业信息';
 
 drop table if exists ws_flink_job_instance;
@@ -323,9 +323,8 @@ CREATE TABLE ws_flink_kubernetes_template
 (
     id          bigint      not null auto_increment,
     `name`      varchar(64) not null,
-    metadata    text comment 'flink deployment metadata',
-    spec        text comment 'flink deployment spec',
-    remark      varchar(255),
+    metadata    text comment 'flink metadata',
+    spec        text comment 'flink spec',
     creator     varchar(32),
     create_time datetime    not null default current_timestamp,
     editor      varchar(32),
@@ -360,35 +359,6 @@ CREATE TABLE ws_flink_kubernetes_deployment
     PRIMARY KEY (id),
     UNIQUE KEY uniq_name (kind, `name`)
 ) ENGINE = INNODB COMMENT = 'flink kubernetes deployment';
-
-DROP TABLE IF EXISTS ws_flink_kubernetes_session_cluster;
-CREATE TABLE ws_flink_kubernetes_session_cluster
-(
-    id                  bigint       not null auto_increment,
-    `name`              varchar(255) not null,
-    namespace           varchar(255) not null,
-    kuberenetes_options varchar(255),
-    job_manager         text,
-    task_manager        text,
-    pod_template        text,
-    flink_configuration text,
-    remark              varchar(255),
-    creator             varchar(32),
-    create_time         datetime     not null default current_timestamp,
-    editor              varchar(32),
-    update_time         datetime     not null default current_timestamp on update current_timestamp,
-    PRIMARY KEY (id),
-    UNIQUE KEY uniq_name (`name`)
-) ENGINE = INNODB COMMENT = 'flink kubernetes session cluster';
-
-INSERT INTO `ws_flink_kubernetes_session_cluster` (`id`, `name`, `namespace`, `kuberenetes_options`, `job_manager`,
-                                                   `task_manager`, `pod_template`, `flink_configuration`,
-                                                   `remark`, `creator`, `editor`)
-VALUES (1, 'session-cluster', 'default',
-        '{\"image\":\"flink:1.15\",\"flinkVersion\":\"v1_15\",\"serviceAccount\":\"flink\"}',
-        '{\"resource\":{\"memory\":\"2048m\",\"cpu\":1}}', '{\"resource\":{\"memory\":\"2048m\",\"cpu\":1}}', NULL,
-        '{\"taskmanager.numberOfTaskSlots\":\"32\"}', NULL,
-        'sys', 'sys');
 
 INSERT INTO `ws_flink_kubernetes_deployment` (`id`, `kind`, `name`, `namespace`, `kuberenetes_options`, `job_manager`,
                                               `task_manager`, `pod_template`, `flink_configuration`, `deployment_name`,
@@ -426,3 +396,29 @@ VALUES (3, 'FlinkDeployment', 'stateful-example2', 'default',
         NULL,
         '{\"jarURI\":\"local:///opt/flink/examples/streaming/StateMachineExample.jar\",\"parallelism\":2,\"entryClass\":\"org.apache.flink.streaming.examples.statemachine.StateMachineExample\",\"args\":[],\"state\":\"running\",\"upgradeMode\":\"last-state\"}',
         NULL, 'sys', 'sys');
+
+
+DROP TABLE IF EXISTS ws_flink_kubernetes_session_cluster;
+CREATE TABLE ws_flink_kubernetes_session_cluster
+(
+    id                    bigint       not null auto_increment,
+    cluster_credential_id bigint       not null,
+    `name`                varchar(255) not null,
+    metadata              text comment 'flink metadata',
+    spec                  text comment 'flink spec',
+    creator               varchar(32),
+    create_time           datetime     not null default current_timestamp,
+    editor                varchar(32),
+    update_time           datetime     not null default current_timestamp on update current_timestamp,
+    PRIMARY KEY (id),
+    UNIQUE KEY uniq_name (`name`)
+) ENGINE = INNODB COMMENT = 'flink kubernetes session cluster';
+
+# INSERT INTO `ws_flink_kubernetes_session_cluster` (`id`, `name`, `namespace`, `kuberenetes_options`, `job_manager`,
+#                                                    `task_manager`, `pod_template`, `flink_configuration`,
+#                                                    `remark`, `creator`, `editor`)
+# VALUES (1, 'session-cluster', 'default',
+#         '{\"image\":\"flink:1.15\",\"flinkVersion\":\"v1_15\",\"serviceAccount\":\"flink\"}',
+#         '{\"resource\":{\"memory\":\"2048m\",\"cpu\":1}}', '{\"resource\":{\"memory\":\"2048m\",\"cpu\":1}}', NULL,
+#         '{\"taskmanager.numberOfTaskSlots\":\"32\"}', NULL,
+#         'sys', 'sys');
