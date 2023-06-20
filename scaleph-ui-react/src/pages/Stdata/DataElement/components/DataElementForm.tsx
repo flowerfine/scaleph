b69f27cc8ef0e13bbabadd33f32ddac27e7880f7
zhang_dechong <!@QW12qw1>
2023-06-20 20:10:16 +0800

    Data Standard - Data Objects menu to add data object errors

--- scaleph-ui-react/src/pages/Stdata/DataElement/components/DataElementForm.tsx
+++ scaleph-ui-react/src/pages/Stdata/DataElement/components/DataElementForm.tsx
@@ -76,7 +76,7 @@ const MetaDataElementForm: React.FC<ModalFormProps<MetaDataElement>> = ({
           id: data.id,
           elementCode: data.elementCode,
           elementName: data.elementName,
-          dataType: data.dataType,
+          dataType: data.dataType?.value,
           dataLength: data.dataLength,
           dataPrecision: data.dataPrecision,
           dataScale: data.dataScale,
