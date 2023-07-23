2c88c6e4a541fae3e7d980b734ca551c3d9fa529
liubodong <liubodong2010@126.com>
2023-07-23 12:43:06 +0800

    [Feature] Refactor flink-sql-gateway backend

--- scaleph-api/src/main/java/cn/sliew/scaleph/api/controller/ws/WsFlinkSqlGatewayController.java
+++ scaleph-api/src/main/java/cn/sliew/scaleph/api/controller/ws/WsFlinkSqlGatewayController.java
@@ -20,21 +20,19 @@ package cn.sliew.scaleph.api.controller.ws;
 
 import cn.sliew.scaleph.engine.sql.gateway.dto.WsFlinkSqlGatewayQueryParamsDTO;
 import cn.sliew.scaleph.engine.sql.gateway.dto.WsFlinkSqlGatewayQueryResultDTO;
+import cn.sliew.scaleph.engine.sql.gateway.dto.catalog.CatalogInfo;
 import cn.sliew.scaleph.engine.sql.gateway.services.WsFlinkSqlGatewayService;
 import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.tags.Tag;
-import org.apache.flink.table.catalog.CatalogBaseTable;
-import org.apache.flink.table.functions.FunctionKind;
 import org.apache.flink.table.gateway.api.results.GatewayInfo;
 import org.apache.flink.table.gateway.api.results.ResultSet;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
-import java.util.Map;
+import java.util.List;
 import java.util.Optional;
 import java.util.Set;
-import java.util.stream.Collectors;
 
 @Tag(name = "Flink Sql Gateway")
 @RestController
@@ -62,81 +60,20 @@ public class WsFlinkSqlGatewayController {
 
     @DeleteMapping("{clusterId}/{sessionHandleId}")
     @Operation(summary = "关闭Session", description = "关闭Session")
-    public ResponseEntity<String> closeSession(@PathVariable("clusterId") String clusterId, @PathVariable("sessionHandleId") String sessionHandleId) {
+    public ResponseEntity<String> closeSession(
+            @PathVariable("clusterId") String clusterId,
+            @PathVariable("sessionHandleId") String sessionHandleId) {
         return ResponseEntity.ok(wsFlinkSqlGatewayService.closeSession(clusterId, sessionHandleId));
     }
 
-    @GetMapping("{clusterId}/{sessionHandleId}/listCatalogs")
-    @Operation(summary = "列出Catalog", description = "列出当前Session下所有的Catalog")
-    public ResponseEntity<Set<String>> listCatalogs(@PathVariable("clusterId") String clusterId, @PathVariable("sessionHandleId") String sessionHandleId) {
-        return ResponseEntity.ok(wsFlinkSqlGatewayService.listCatalogs(clusterId, sessionHandleId));
+    @GetMapping("{clusterId}/{sessionHandleId}/getCatalogInfo")
+    @Operation(summary = "获取所有Catalog信息", description = "获取所有Catalog信息")
+    public ResponseEntity<Set<CatalogInfo>> getCatalogInfo(
+            @PathVariable("clusterId") String clusterId,
+            @PathVariable("sessionHandleId") String sessionHandleId) {
+        return ResponseEntity.ok(wsFlinkSqlGatewayService.getCatalogInfo(clusterId, sessionHandleId));
     }
 
-    @GetMapping("{clusterId}/{sessionHandleId}/listDatabases")
-    @Operation(summary = "列出Catalog下所有的数据库", description = "列出Catalog下所有的数据库，如果Catalog为空，则列出当前Catalog下的数据库")
-    public ResponseEntity<Set<String>> listDatabases(@PathVariable("clusterId") String clusterId,
-                                                     @PathVariable("sessionHandleId") String sessionHandleId,
-                                                     @RequestParam("catalog") String catalog) {
-        return ResponseEntity.ok(wsFlinkSqlGatewayService.listDatabases(clusterId, sessionHandleId, catalog));
-    }
-
-    @GetMapping("{clusterId}/{sessionHandleId}/listTables")
-    @Operation(summary = "列出Catalog/Database下所有的表", description = "列出Catalog/Database下所有表和视图")
-    public ResponseEntity<Set<String>> listTables(@PathVariable("clusterId") String clusterId,
-                                                  @PathVariable("sessionHandleId") String sessionHandleId,
-                                                  @RequestParam("catalog") String catalog,
-                                                  @RequestParam("database") String database) {
-        return ResponseEntity.ok(wsFlinkSqlGatewayService
-                .listTables(clusterId, sessionHandleId, catalog, database, Set.of(CatalogBaseTable.TableKind.TABLE))
-                .stream()
-                .map(tableInfo -> tableInfo.getIdentifier().getObjectName())
-                .collect(Collectors.toSet())
-        );
-    }
-
-    @GetMapping("{clusterId}/{sessionHandleId}/listViews")
-    @Operation(summary = "列出Catalog/Database下所有的表", description = "列出Catalog/Database下所有表和视图")
-    public ResponseEntity<Set<String>> listViews(@PathVariable("clusterId") String clusterId,
-                                                 @PathVariable("sessionHandleId") String sessionHandleId,
-                                                 @RequestParam("catalog") String catalog,
-                                                 @RequestParam("database") String database) {
-        return ResponseEntity.ok(wsFlinkSqlGatewayService
-                .listTables(clusterId, sessionHandleId, catalog, database, Set.of(CatalogBaseTable.TableKind.VIEW))
-                .stream()
-                .map(tableInfo -> tableInfo.getIdentifier().getObjectName())
-                .collect(Collectors.toSet())
-        );
-    }
-
-    @GetMapping("{clusterId}/{sessionHandleId}/listSystemFunctions")
-    @Operation(summary = "列出系统方法", description = "列出系统方法")
-    public ResponseEntity<Set<Object>> listSystemFunctions(@PathVariable("clusterId") String clusterId,
-                                                           @PathVariable("sessionHandleId") String sessionHandleId) {
-        return ResponseEntity.ok(
-                wsFlinkSqlGatewayService.listSystemFunctions(clusterId, sessionHandleId)
-                        .stream()
-                        .map(functionInfo ->
-                                Map.of("functionName", functionInfo.getIdentifier().getFunctionName(),
-                                        "functionKind", functionInfo.getKind().orElse(FunctionKind.OTHER).name())
-                        ).collect(Collectors.toSet())
-        );
-    }
-
-    @GetMapping("{clusterId}/{sessionHandleId}/listUserDefinedFunctions")
-    @Operation(summary = "列出用户自定义方法", description = "列出用户自定义方法")
-    public ResponseEntity<Set<Object>> listUserDefinedFunctions(@PathVariable("clusterId") String clusterId,
-                                                                @PathVariable("sessionHandleId") String sessionHandleId,
-                                                                @RequestParam("catalog") String catalog,
-                                                                @RequestParam("database") String database) {
-        return ResponseEntity.ok(
-                wsFlinkSqlGatewayService.listUserDefinedFunctions(clusterId, sessionHandleId, catalog, database)
-                        .stream()
-                        .map(functionInfo ->
-                                Map.of("functionName", functionInfo.getIdentifier().getFunctionName(),
-                                        "functionKind", functionInfo.getKind().orElse(FunctionKind.OTHER).name())
-                        ).collect(Collectors.toSet())
-        );
-    }
 
     @RequestMapping(value = "{clusterId}/{sessionHandleId}/executeSql", method = {RequestMethod.POST, RequestMethod.PUT})
     @Operation(summary = "执行FlinkSql", description = "执行FlinkSql")
@@ -168,6 +105,20 @@ public class WsFlinkSqlGatewayController {
                                           @PathVariable("sessionHandleId") String sessionHandleId,
                                           @PathVariable("operationHandleId") String operationHandleId) {
         return ResponseEntity.ok(wsFlinkSqlGatewayService.cancel(clusterId, sessionHandleId, operationHandleId));
+    }
+
+    @GetMapping("{clusterId}/{sessionHandleId}/completeStatement")
+    @Operation(summary = "获取Sql提示", description = "获取Sql提示")
+    public ResponseEntity<List<String>> completeStatement(
+            @PathVariable("clusterId") String clusterId,
+            @PathVariable("sessionHandleId") String sessionHandleId,
+            @RequestParam("statement") String statement,
+            @RequestParam("position") int position) {
+        try {
+            return ResponseEntity.ok(wsFlinkSqlGatewayService.completeStatement(clusterId, sessionHandleId, statement, position));
+        } catch (Exception e) {
+            return ResponseEntity.badRequest().build();
+        }
     }
 
 }
