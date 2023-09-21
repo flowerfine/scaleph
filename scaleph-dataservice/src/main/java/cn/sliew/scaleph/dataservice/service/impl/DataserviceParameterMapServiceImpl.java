c415fbdb388fe2e47c42bcb01aeb76350d9dd201
wangqi <1942460489@qq.com>
2023-09-21 21:25:33 +0800

    feature: dataservice parameter and result map service

---
+++ scaleph-dataservice/src/main/java/cn/sliew/scaleph/dataservice/service/impl/DataserviceParameterMapServiceImpl.java
@@ -0,0 +1,88 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements.  See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership.  The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License.  You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package cn.sliew.scaleph.dataservice.service.impl;
+
+import cn.sliew.scaleph.common.util.BeanUtil;
+import cn.sliew.scaleph.dao.entity.master.dataservice.DataserviceParameterMap;
+import cn.sliew.scaleph.dao.mapper.master.dataservice.DataserviceParameterMapMapper;
+import cn.sliew.scaleph.dataservice.service.DataserviceParameterMapService;
+import cn.sliew.scaleph.dataservice.service.convert.DataserviceParameterMapConvert;
+import cn.sliew.scaleph.dataservice.service.dto.DataserviceParameterMapDTO;
+import cn.sliew.scaleph.dataservice.service.param.DataserviceParameterMapAddParam;
+import cn.sliew.scaleph.dataservice.service.param.DataserviceParameterMapListParam;
+import cn.sliew.scaleph.dataservice.service.param.DataserviceParameterMapUpdateParam;
+import com.baomidou.mybatisplus.core.conditions.query.LambdaQueryWrapper;
+import com.baomidou.mybatisplus.core.toolkit.Wrappers;
+import com.baomidou.mybatisplus.extension.plugins.pagination.Page;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.util.StringUtils;
+
+import java.util.List;
+
+import static cn.sliew.milky.common.check.Ensures.checkState;
+
+@Service
+public class DataserviceParameterMapServiceImpl implements DataserviceParameterMapService {
+
+    @Autowired
+    private DataserviceParameterMapMapper dataserviceParameterMapMapper;
+
+    @Override
+    public Page<DataserviceParameterMapDTO> list(DataserviceParameterMapListParam param) {
+        Page<DataserviceParameterMap> page = new Page<>(param.getCurrent(), param.getPageSize());
+        LambdaQueryWrapper<DataserviceParameterMap> queryWrapper = Wrappers.lambdaQuery(DataserviceParameterMap.class)
+                .like(StringUtils.hasText(param.getName()), DataserviceParameterMap::getName, param.getName());
+
+        Page<DataserviceParameterMap> dataserviceParameterMapPage = dataserviceParameterMapMapper.selectPage(page, queryWrapper);
+        Page<DataserviceParameterMapDTO> result = new Page<>(dataserviceParameterMapPage.getCurrent(), dataserviceParameterMapPage.getSize(), dataserviceParameterMapPage.getTotal());
+        List<DataserviceParameterMapDTO> dataserviceParameterMapDTOS = DataserviceParameterMapConvert.INSTANCE.toDto(dataserviceParameterMapPage.getRecords());
+        result.setRecords(dataserviceParameterMapDTOS);
+        return result;
+    }
+
+    @Override
+    public DataserviceParameterMapDTO selectOne(Long id) {
+        DataserviceParameterMap record = dataserviceParameterMapMapper.selectById(id);
+        checkState(record != null, () -> "data service parameter map not exists for id: " + id);
+        return DataserviceParameterMapConvert.INSTANCE.toDto(record);
+    }
+
+    @Override
+    public int insert(DataserviceParameterMapAddParam param) {
+        DataserviceParameterMap record = BeanUtil.copy(param, new DataserviceParameterMap());
+        return dataserviceParameterMapMapper.insert(record);
+    }
+
+    @Override
+    public int update(DataserviceParameterMapUpdateParam param) {
+        DataserviceParameterMap record = BeanUtil.copy(param, new DataserviceParameterMap());
+        return dataserviceParameterMapMapper.updateById(record);
+    }
+
+    @Override
+    public int deleteById(Long id) {
+        return dataserviceParameterMapMapper.deleteById(id);
+    }
+
+    @Override
+    public int deleteBatch(List<Long> ids) {
+        return dataserviceParameterMapMapper.deleteBatchIds(ids);
+    }
+}
---
+++ scaleph-dataservice/src/main/java/cn/sliew/scaleph/dataservice/service/impl/DataserviceResultMapServiceImpl.java
@@ -0,0 +1,88 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements.  See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership.  The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License.  You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package cn.sliew.scaleph.dataservice.service.impl;
+
+import cn.sliew.scaleph.common.util.BeanUtil;
+import cn.sliew.scaleph.dao.entity.master.dataservice.DataserviceResultMap;
+import cn.sliew.scaleph.dao.mapper.master.dataservice.DataserviceResultMapMapper;
+import cn.sliew.scaleph.dataservice.service.DataserviceResultMapService;
+import cn.sliew.scaleph.dataservice.service.convert.DataserviceResultMapConvert;
+import cn.sliew.scaleph.dataservice.service.dto.DataserviceResultMapDTO;
+import cn.sliew.scaleph.dataservice.service.param.DataserviceConfigListParam;
+import cn.sliew.scaleph.dataservice.service.param.DataserviceResultMapAddParam;
+import cn.sliew.scaleph.dataservice.service.param.DataserviceResultMapUpdateParam;
+import com.baomidou.mybatisplus.core.conditions.query.LambdaQueryWrapper;
+import com.baomidou.mybatisplus.core.toolkit.Wrappers;
+import com.baomidou.mybatisplus.extension.plugins.pagination.Page;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.util.StringUtils;
+
+import java.util.List;
+
+import static cn.sliew.milky.common.check.Ensures.checkState;
+
+@Service
+public class DataserviceResultMapServiceImpl implements DataserviceResultMapService {
+
+    @Autowired
+    private DataserviceResultMapMapper dataserviceResultMapMapper;
+
+    @Override
+    public Page<DataserviceResultMapDTO> list(DataserviceConfigListParam param) {
+        Page<DataserviceResultMap> page = new Page<>(param.getCurrent(), param.getPageSize());
+        LambdaQueryWrapper<DataserviceResultMap> queryWrapper = Wrappers.lambdaQuery(DataserviceResultMap.class)
+                .like(StringUtils.hasText(param.getName()), DataserviceResultMap::getName, param.getName());
+
+        Page<DataserviceResultMap> dataserviceResultMapPage = dataserviceResultMapMapper.selectPage(page, queryWrapper);
+        Page<DataserviceResultMapDTO> result = new Page<>(dataserviceResultMapPage.getCurrent(), dataserviceResultMapPage.getSize(), dataserviceResultMapPage.getTotal());
+        List<DataserviceResultMapDTO> dataserviceResultMapDTOS = DataserviceResultMapConvert.INSTANCE.toDto(dataserviceResultMapPage.getRecords());
+        result.setRecords(dataserviceResultMapDTOS);
+        return result;
+    }
+
+    @Override
+    public DataserviceResultMapDTO selectOne(Long id) {
+        DataserviceResultMap record = dataserviceResultMapMapper.selectById(id);
+        checkState(record != null, () -> "data service result map not exists for id: " + id);
+        return DataserviceResultMapConvert.INSTANCE.toDto(record);
+    }
+
+    @Override
+    public int insert(DataserviceResultMapAddParam param) {
+        DataserviceResultMap record = BeanUtil.copy(param, new DataserviceResultMap());
+        return dataserviceResultMapMapper.insert(record);
+    }
+
+    @Override
+    public int update(DataserviceResultMapUpdateParam param) {
+        DataserviceResultMap record = BeanUtil.copy(param, new DataserviceResultMap());
+        return dataserviceResultMapMapper.updateById(record);
+    }
+
+    @Override
+    public int deleteById(Long id) {
+        return dataserviceResultMapMapper.deleteById(id);
+    }
+
+    @Override
+    public int deleteBatch(List<Long> ids) {
+        return dataserviceResultMapMapper.deleteBatchIds(ids);
+    }
+}
