672e727343843977d107a468a8f584c251b6e91b
wangqi <1942460489@qq.com>
2023-11-13 21:20:42 +0800

    feature: add web-resource and role relation mapper

---
+++ scaleph-dao/src/main/java/cn/sliew/scaleph/dao/entity/master/security/SecResourceWebRole.java
@@ -0,0 +1,42 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements.  See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership.  The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License.  You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package cn.sliew.scaleph.dao.entity.master.security;
+
+import cn.sliew.scaleph.dao.entity.BaseDO;
+import com.baomidou.mybatisplus.annotation.TableField;
+import com.baomidou.mybatisplus.annotation.TableName;
+import io.swagger.v3.oas.annotations.media.Schema;
+import lombok.Data;
+
+/**
+ * 资源-web与角色关联表
+ */
+@Data
+@TableName("sec_resource_web_role")
+@Schema(name = "SecResourceWebRole对象", description = "资源-web与角色关联表")
+public class SecResourceWebRole extends BaseDO {
+
+    private static final long serialVersionUID = 1L;
+
+    @TableField("resource_web_id")
+    private Long resourceWebId;
+
+    @TableField("role_id")
+    private Long roleId;
+}
