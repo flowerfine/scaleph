576a25ebe07011addd33d14407c1e8c962a08abd
wangqi <1942460489@qq.com>
2023-11-26 11:05:35 +0800

    fix: doris template step form item error

--- scaleph-ui-react/src/pages/Project/Workspace/Doris/Template/Steps/Component/DorisAdminUser.tsx
+++ scaleph-ui-react/src/pages/Project/Workspace/Doris/Template/Steps/Component/DorisAdminUser.tsx
@@ -8,20 +8,22 @@ const DorisAdminUser: React.FC = () => {
     title={intl.formatMessage({id: 'pages.project.doris.template.steps.component.admin'})}
     headerBordered
     collapsible={true}>
-    <ProFormText
-      name="admin.name"
-      label={intl.formatMessage({id: 'pages.project.doris.template.steps.component.admin.name'})}
-      colProps={{span: 10, offset: 1}}
-      rules={[{required: true}]}
-      initialValue={"admin"}
-    />
-    <ProFormText
-      name="admin.password"
-      label={intl.formatMessage({id: 'pages.project.doris.template.steps.component.admin.password'})}
-      colProps={{span: 10, offset: 1}}
-      rules={[{required: true}]}
-      initialValue={"Admin123"}
-    />
+    <ProFormGroup>
+      <ProFormText
+        name="admin.name"
+        label={intl.formatMessage({id: 'pages.project.doris.template.steps.component.admin.name'})}
+        colProps={{span: 10, offset: 1}}
+        rules={[{required: true}]}
+        initialValue={"admin"}
+      />
+      <ProFormText
+        name="admin.password"
+        label={intl.formatMessage({id: 'pages.project.doris.template.steps.component.admin.password'})}
+        colProps={{span: 10, offset: 1}}
+        rules={[{required: true}]}
+        initialValue={"Admin123"}
+      />
+    </ProFormGroup>
   </ProCard>);
 }
 
