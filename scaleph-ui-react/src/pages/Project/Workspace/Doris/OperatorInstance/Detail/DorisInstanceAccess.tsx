45db944cd48e65047dc6e4d37967526daff8cfe8
wangqi <wangqi@xinxuan.net>
2023-12-20 17:56:45 +0800

    feature: doris instance detail state

--- scaleph-ui-react/src/pages/Project/Workspace/Doris/OperatorInstance/Detail/DorisInstanceAccess.tsx
+++ scaleph-ui-react/src/pages/Project/Workspace/Doris/OperatorInstance/Detail/DorisInstanceAccess.tsx
@@ -10,7 +10,10 @@ const DorisInstanceDetailAccess: React.FC<{ data: WsDorisOperatorInstance }> = (
     <ProCard.Group title={intl.formatMessage({id: 'pages.project.doris.instance.detail.access'})}
                    direction={'row'}>
       <ProCard bordered>
-      集群连接信息，敬请期待~
+      k8s内部连接信息，敬请期待~
+      </ProCard>
+      <ProCard bordered>
+        k8s外部连接信息，敬请期待~
       </ProCard>
       </ProCard.Group>
       );
