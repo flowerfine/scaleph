ea4c0475cfe2b144f2d8ef484cc21cc09940ad1f
wangqi <1942460489@qq.com>
2024-03-07 22:29:37 +0800

    feature: upgrade seatunnel kudu connectors to 2.3.4

--- scaleph-plugins/scaleph-plugin-seatunnel-connectors/src/main/java/cn/sliew/scaleph/plugin/seatunnel/flink/connectors/kudu/KuduProperties.java
+++ scaleph-plugins/scaleph-plugin-seatunnel-connectors/src/main/java/cn/sliew/scaleph/plugin/seatunnel/flink/connectors/kudu/KuduProperties.java
@@ -19,6 +19,7 @@
 package cn.sliew.scaleph.plugin.seatunnel.flink.connectors.kudu;
 
 import cn.sliew.scaleph.plugin.framework.property.*;
+import com.fasterxml.jackson.databind.JsonNode;
 
 public enum KuduProperties {
     ;
@@ -46,6 +47,94 @@ public enum KuduProperties {
             .type(PropertyType.STRING)
             .parser(Parsers.STRING_PARSER)
             .properties(Property.Required)
+            .addValidator(Validators.NON_BLANK_VALIDATOR)
+            .validateAndBuild();
+
+    public static final PropertyDescriptor<Integer> CLIENT_WORKER_COUNT = new PropertyDescriptor.Builder()
+            .name("client_worker_count")
+            .description("Kudu worker count. Default value is twice the current number of cpu cores.")
+            .type(PropertyType.INT)
+            .parser(Parsers.INTEGER_PARSER)
+            .addValidator(Validators.POSITIVE_INTEGER_VALIDATOR)
+            .validateAndBuild();
+
+    public static final PropertyDescriptor<Long> CLIENT_DEFAULT_OPERATION_TIMEOUT_MS = new PropertyDescriptor.Builder()
+            .name("client_default_operation_timeout_ms")
+            .description("Kudu normal operation timeout.")
+            .type(PropertyType.INT)
+            .parser(Parsers.LONG_PARSER)
+            .addValidator(Validators.POSITIVE_LONG_VALIDATOR)
+            .validateAndBuild();
+
+    public static final PropertyDescriptor<Long> CLIENT_DEFAULT_ADMIN_OPERATION_TIMEOUT_MS = new PropertyDescriptor.Builder()
+            .name("client_default_admin_operation_timeout_ms")
+            .description("Kudu admin operation timeout.")
+            .type(PropertyType.INT)
+            .parser(Parsers.LONG_PARSER)
+            .addValidator(Validators.POSITIVE_LONG_VALIDATOR)
+            .validateAndBuild();
+
+    public static final PropertyDescriptor<Boolean> ENABLE_KERBEROS = new PropertyDescriptor.Builder()
+            .name("enable_kerberos")
+            .description("Kerberos principal enable.")
+            .type(PropertyType.BOOLEAN)
+            .parser(Parsers.BOOLEAN_PARSER)
+            .addValidator(Validators.BOOLEAN_VALIDATOR)
+            .validateAndBuild();
+
+    public static final PropertyDescriptor<String> KERBEROS_PRINCIPAL = new PropertyDescriptor.Builder()
+            .name("kerberos_principal")
+            .description("Kerberos principal.")
+            .type(PropertyType.STRING)
+            .parser(Parsers.STRING_PARSER)
+            .addValidator(Validators.NON_BLANK_VALIDATOR)
+            .validateAndBuild();
+
+    public static final PropertyDescriptor<String> KERBEROS_KEYTAB = new PropertyDescriptor.Builder()
+            .name("kerberos_keytab")
+            .description("Kerberos keytab.")
+            .type(PropertyType.STRING)
+            .parser(Parsers.STRING_PARSER)
+            .addValidator(Validators.NON_BLANK_VALIDATOR)
+            .validateAndBuild();
+
+    public static final PropertyDescriptor<String> KERBEROS_KRB5CONF = new PropertyDescriptor.Builder()
+            .name("kerberos_krb5conf")
+            .description("Kerberos krb5 conf.")
+            .type(PropertyType.STRING)
+            .parser(Parsers.STRING_PARSER)
+            .addValidator(Validators.NON_BLANK_VALIDATOR)
+            .validateAndBuild();
+
+    public static final PropertyDescriptor<Long> SCAN_TOKEN_QUERY_TIMEOUT = new PropertyDescriptor.Builder()
+            .name("scan_token_query_timeout")
+            .description("The timeout for connecting scan token.")
+            .type(PropertyType.INT)
+            .parser(Parsers.LONG_PARSER)
+            .addValidator(Validators.POSITIVE_LONG_VALIDATOR)
+            .validateAndBuild();
+
+    public static final PropertyDescriptor<Integer> SCAN_TOKEN_BATCH_SIZE_BYTES = new PropertyDescriptor.Builder()
+            .name("scan_token_batch_size_bytes")
+            .description("Kudu scan bytes.")
+            .type(PropertyType.INT)
+            .parser(Parsers.INTEGER_PARSER)
+            .addValidator(Validators.POSITIVE_INTEGER_VALIDATOR)
+            .validateAndBuild();
+
+    public static final PropertyDescriptor<JsonNode> SCHEMA = new PropertyDescriptor.Builder()
+            .name("schema")
+            .description("SeaTunnel Schema.")
+            .type(PropertyType.OBJECT)
+            .parser(Parsers.JSON_PARSER)
+            .addValidator(Validators.NON_BLANK_VALIDATOR)
+            .validateAndBuild();
+
+    public static final PropertyDescriptor<JsonNode> TABLE_LIST = new PropertyDescriptor.Builder()
+            .name("table_list")
+            .description("The list of tables to be read.")
+            .type(PropertyType.OBJECT)
+            .parser(Parsers.JSON_PARSER)
             .addValidator(Validators.NON_BLANK_VALIDATOR)
             .validateAndBuild();
 
