c340bd34547f6cbf0a5beac804bd946f2fb9a0a3
wangqi <wangqi@xinxuan.net>
2024-04-16 16:18:34 +0800

    feature: add dag config mysql schema

--- tools/docker/mysql/init.d/scaleph-dag-mysql.sql
+++ tools/docker/mysql/init.d/scaleph-dag-mysql.sql
@@ -1,6 +1,90 @@
 create database if not exists scaleph default character set utf8mb4 collate utf8mb4_unicode_ci;
 use scaleph;
 
+drop table if exists dag_config;
+create table dag_config
+(
+    id             bigint      not null auto_increment comment '自增主键',
+    type           varchar(16) not null comment 'DAG 类型',
+    name           varchar(128) comment 'DAG名称',
+    config_id      varchar(64) not null comment 'DAG ID',
+    dag_meta       varchar(128) comment 'DAG元信息',
+    dag_attrs      mediumtext comment 'DAG属性',
+    intput_options text comment '输入参数声明',
+    output_options text comment '输出参数声明',
+    version        int         not null default 0 comment '版本号',
+    remark         varchar(255) comment '备注',
+    creator        varchar(32) comment '创建人',
+    create_time    timestamp            default current_timestamp comment '创建时间',
+    editor         varchar(32) comment '修改人',
+    update_time    timestamp            default current_timestamp on update current_timestamp comment '修改时间',
+    primary key (id)
+) engine = innodb comment 'DAG 配置';
+
+drop table if exists dag_config_history;
+create table dag_config_history
+(
+    id             bigint      not null auto_increment comment '自增主键',
+    dag_config_id  bigint      not null comment 'DAG 配置ID',
+    type           varchar(16) not null comment 'DAG 类型',
+    name           varchar(128) comment 'DAG名称',
+    config_id      varchar(64) not null comment 'DAG ID',
+    dag_meta       varchar(128) comment 'DAG元信息',
+    dag_attrs      mediumtext comment 'DAG属性',
+    intput_options text comment '输入参数声明',
+    output_options text comment '输出参数声明',
+    version        int         not null default 0 comment '版本号',
+    remark         varchar(255) comment '备注',
+    creator        varchar(32) comment '创建人',
+    create_time    timestamp            default current_timestamp comment '创建时间',
+    editor         varchar(32) comment '修改人',
+    update_time    timestamp            default current_timestamp on update current_timestamp comment '修改时间',
+    primary key (id),
+    key            idx_dag_config (`dag_config_id`)
+) engine = innodb comment 'DAG 配置历史。使用 javers 管理';
+
+drop table if exists dag_config_step;
+create table dag_config_step
+(
+    id          bigint      not null auto_increment comment '自增主键',
+    dag_id      bigint      not null comment 'DAG id',
+    step_id     varchar(36) not null comment '步骤id',
+    step_name   varchar(128) comment '步骤名称',
+    position_x  int         not null comment 'x坐标',
+    position_y  int         not null comment 'y坐标',
+    shape       varchar(64),
+    style       text,
+    step_meta   varchar(128) comment '步骤元信息',
+    step_attrs  mediumtext comment '步骤属性',
+    creator     varchar(32) comment '创建人',
+    create_time timestamp default current_timestamp comment '创建时间',
+    editor      varchar(32) comment '修改人',
+    update_time timestamp default current_timestamp on update current_timestamp comment '修改时间',
+    primary key (id),
+    unique key uniq_step (dag_id, step_id)
+) engine = innodb comment 'DAG 配置步骤';
+
+drop table if exists dag_config_link;
+create table dag_config_link
+(
+    id           bigint      not null auto_increment comment '自增主键',
+    dag_id       bigint      not null comment 'DAG id',
+    link_id      varchar(36) not null comment '连线id',
+    link_name    varchar(128) comment '连线名称',
+    from_step_id varchar(36) not null comment '源步骤id',
+    to_step_id   varchar(36) not null comment '目标步骤id',
+    shape        varchar(64),
+    style        text,
+    link_meta    varchar(128) comment '连线元信息',
+    link_attrs   mediumtext comment '连线属性',
+    creator      varchar(32) comment '创建人',
+    create_time  timestamp default current_timestamp comment '创建时间',
+    editor       varchar(32) comment '修改人',
+    update_time  timestamp default current_timestamp on update current_timestamp comment '修改时间',
+    primary key (id),
+    unique key uniq_link (dag_id, link_id)
+) engine = innodb comment 'DAG 配置连线';
+
 drop table if exists dag_instance;
 create table dag_instance
 (
