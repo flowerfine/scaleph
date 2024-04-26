e075ef7dfad2fd22b852b87cb55dbae71e689434
wangqi <1942460489@qq.com>
2024-04-26 09:00:40 +0800

    feature: add oam mapper

---
+++ scaleph-dao/src/main/java/cn/sliew/scaleph/dao/entity/master/oam/OamWorkloadDefinition.java
@@ -0,0 +1,56 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements.  See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership.  The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License.  You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package cn.sliew.scaleph.dao.entity.master.oam;
+
+import cn.sliew.scaleph.dao.entity.BaseDO;
+import com.baomidou.mybatisplus.annotation.TableField;
+import com.baomidou.mybatisplus.annotation.TableName;
+import io.swagger.v3.oas.annotations.media.Schema;
+import lombok.Data;
+
+/**
+ * Workload Definition 信息
+ */
+@Data
+@TableName("oam_workload_definition")
+@Schema(name = "OamWorkloadDefinition", description = "Workload Definition 信息")
+public class OamWorkloadDefinition extends BaseDO {
+
+    private static final long serialVersionUID = 1L;
+
+    @Schema(description = "定义id。主要用于 kubernetes 中 metadata 使用")
+    @TableField("definition_id")
+    private String definitionId;
+
+    @TableField("`name`")
+    private String name;
+
+    @TableField("definition_ref")
+    private String definitionRef;
+
+    @TableField("schematic")
+    private String schematic;
+
+    @TableField("extension")
+    private String extension;
+
+    @Schema(description = "备注")
+    @TableField("remark")
+    private String remark;
+}
