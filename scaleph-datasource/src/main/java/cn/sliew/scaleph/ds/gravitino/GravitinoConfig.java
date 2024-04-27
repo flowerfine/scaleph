e4c3e61403c3009ddb82032f4b98f09d3b4a5d23
wangqi <1942460489@qq.com>
2024-04-27 21:21:53 +0800

    feature: add oam definition web

--- scaleph-datasource/src/main/java/cn/sliew/scaleph/ds/gravitino/GravitinoConfig.java
+++ scaleph-datasource/src/main/java/cn/sliew/scaleph/ds/gravitino/GravitinoConfig.java
@@ -25,8 +25,8 @@ import org.springframework.boot.context.properties.EnableConfigurationProperties
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
-//@Configuration
-//@EnableConfigurationProperties(GravitinoProperties.class)
+@Configuration
+@EnableConfigurationProperties(GravitinoProperties.class)
 public class GravitinoConfig {
 
     @Autowired
